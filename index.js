b793e7c904acd97f1b187ac8e30c898e2e09abea
MNicolasPP <a01255150@tec.mx>
2025-11-04 14:50:26 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.listen(port);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+console.log('server on port', port);
