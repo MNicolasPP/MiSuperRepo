1620183ea6a2da6b0766993196d8753f18d441d4
MNicolasPP <a01255150@tec.mx>
2025-11-11 15:29:24 -0700

    Esto se supone que no sirve

--- src/db.js
+++ src/db.js
@@ -1,32 +1,30 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const puul = new Pool({
-    coonectionString: AudioProcessingEvent.env.DATABASE_URL
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
 });
 
-
 export const query = (text, params) => {
-    return Pool.query(text, params);
+    return pool.query(text, params);
 };
 
 const initializeDatabase = async () => {
     try {
         await query(`
-            CREATE TABLE IF NOT EXISTS posts {
-            id SERIAL PRIMARY KEY, 
+        CREATE TABLE IF NOT EXISTS posts (
+            id SERIAL PRIMARY KEY,
             content TEXT NOT NULL,
-            created_at TIMESAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESAMP DEFAULT CURRENT_TIMESTAMP
-            };
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
         `);
-        console.log('database tables created or already exist.')
+        console.log('database tables created or already exist.');
     } catch (error) {
-        console.log('Error initializing the databases', error);
+        console.error('Error initializing the databes', error);
     }
 }
 
-
 initializeDatabase();
 
-export default Pool;
+export default pool;
