510236e9440de7945bf24c513b1e9b692115a092
MNicolasPP <a01255150@tec.mx>
2025-11-11 17:06:00 -0700

    Backend ya funcional

--- src/db.js
+++ src/db.js
@@ -1,29 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,              // obliga a usar SSL
+    rejectUnauthorized: false,  // Render usa certificados autofirmados
+  },
 });
 
-export const query = (text, params) => {
-    return pool.query(text, params);
-};
+export const query = (text, params) => pool.query(text, params);
 
 const initializeDatabase = async () => {
-    try {
-        await query(`
-        CREATE TABLE IF NOT EXISTS posts (
-            id SERIAL PRIMARY KEY,
-            content TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-        `);
-        console.log('database tables created or already exist.');
-    } catch (error) {
-        console.error('Error initializing the databes', error);
-    }
-}
+  try {
+    await query(`
+      CREATE TABLE IF NOT EXISTS posts (
+        id SERIAL PRIMARY KEY,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+    console.log('✅ Database tables created or already exist.');
+  } catch (error) {
+    console.error('❌ Error initializing database:', error);
+  }
+};
 
 initializeDatabase();
 
